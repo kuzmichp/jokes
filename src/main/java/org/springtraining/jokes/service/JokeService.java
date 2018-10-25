fad0b5f6edb2de89a98ef7aefff1055cf4b0a984
Paweł Kuźmicz <pavel.kuzmich@syncron.com>
2018-10-25 18:25:49 +0200

    Jokes #3: Create service for jokes

---
+++ src/main/java/org/springtraining/jokes/service/JokeService.java
@@ -0,0 +1,6 @@
+package org.springtraining.jokes.service;
+
+public interface JokeService {
+
+	String getJoke();
+}
---
+++ src/main/java/org/springtraining/jokes/service/JokeServiceImpl.java
@@ -0,0 +1,20 @@
+package org.springtraining.jokes.service;
+
+import org.springframework.stereotype.Service;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+
+@Service
+public class JokeServiceImpl implements JokeService {
+
+	private final ChuckNorrisQuotes quotes;
+
+	public JokeServiceImpl() {
+		quotes = new ChuckNorrisQuotes();
+	}
+
+	@Override
+	public String getJoke() {
+		return quotes.getRandomQuote();
+	}
+}
